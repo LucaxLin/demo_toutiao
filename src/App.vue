dec5422e19c0c9c17502cd1d9023a64893990d4f
lucaslin <892789989@qq.com>
2022-02-16 13:48:03 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <router-view></router-view>
+    <van-tabbar route placeholder>
+      <van-tabbar-item replace to="/index" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item replace to="/user" icon="user-o">标签</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      active: 0
     }
   }
 }
-</style>
+</script>
+
+<style lang="less" scoped></style>
